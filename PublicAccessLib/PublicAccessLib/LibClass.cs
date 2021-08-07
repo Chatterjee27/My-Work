18133bc938bfe472199eddd9107b98b952c47f6a
Chatterjee27 <85678864+Chatterjee27@users.noreply.github.com>
2021-08-07 17:19:31 +0530

    Add the file

---
+++ PublicAccessLib/PublicAccessLib/LibClass.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PublicAccessLib
+{
+    public class LibClass
+    {
+        //private int num;
+       // protected string companyName;
+        internal char gender;
+        public bool isEmployed;
+       
+    }
+}
